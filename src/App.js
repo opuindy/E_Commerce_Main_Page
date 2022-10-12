749ba75d6fb0c5387b70aa29ea3f40b51f99aa1a
Tarry <opuindy@gmail.com>
2022-10-12 12:47:21 +0100

    modified error page route

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const App = () => {
           <Route path='women' element={<Women />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<Error />} />
         </Route>
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
